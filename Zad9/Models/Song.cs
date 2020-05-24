606a7367bf3489646822bb24ed63eda05d5da82e
GitHubcioLX <52084994+GitHubcioLX@users.noreply.github.com>
2020-05-24 22:13:05 +0200

    Zadanie 9.

---
+++ Zad9/Models/Song.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Zadanie9.Models
+{
+    public class Song
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a song is 100 characters!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Name of the artist is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of an artist is 100 characters!")]
+        public string Artist { get; set; }
+
+        public int GenreId { get; set; }
+
+        public Song() {}
+
+        public Song(int id, string name, string artist, int genreId)
+        {
+            Id = id;
+            Name = name;
+            Artist = artist;
+            GenreId = genreId;
+        }
+    }
+}
